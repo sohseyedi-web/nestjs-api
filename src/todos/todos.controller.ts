bf45ab48adc962adb06d6383426026d27eb0cd4a
sohseyedi <sohseyedi@gmail.com>
2023-05-27 21:28:42 +0330

    crud app

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,39 @@
 import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common/decorators';
+import { CreateTodoDto } from './DTO/create-todo.dto';
+import { Todos } from './todos.interface';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+  @Get()
+  findTodo(@Query() createTodoDto: CreateTodoDto): Todos[] {
+    if (Object.keys(createTodoDto).length) {
+      return this.todosService.findTodo(createTodoDto);
+    }
+    return this.todosService.findAll();
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string): Todos {
+    return this.todosService.findById(id);
+  }
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDto): Todos {
+    return this.todosService.createTodo(createTodoDto);
+  }
+
+  @Delete('/:id')
+  deletTodo(@Param('id') id: string): void {
+    this.todosService.deleteTodo(id);
+  }
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Todos } from './todos.interface';
+import { CreateTodoDto } from './DTO/create-todo.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { HttpException } from '@nestjs/common/exceptions';
+import { HttpStatus } from '@nestjs/common/enums';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  private TodoItem: Todos[] = [];
+
+  findAll(): Todos[] {
+    return this.TodoItem;
+  }
+
+  findTodo(createTodoDto: CreateTodoDto): Todos[] {
+    let todos = this.findAll();
+    const { text } = createTodoDto;
+
+    if (text) {
+      todos = todos.filter((i) => i.text.toLowerCase().includes(text));
+    }
+
+    return todos;
+  }
+  findById(id: string): Todos {
+    return this.TodoItem.find((i) => i.id === id);
+  }
+  createTodo(createTodoDto: CreateTodoDto) {
+    const { onCompleted, text } = createTodoDto;
+
+    const todo = {
+      id: uuidv4(),
+      text,
+      onCompleted: false,
+    };
+
+    this.TodoItem.push(todo);
+    return todo;
+  }
+  deleteTodo(id: string): void {
+    this.TodoItem = this.TodoItem.filter((i) => i.id !== id);
+    throw new HttpException('Deleted Todos', HttpStatus.OK);
+  }
+}
